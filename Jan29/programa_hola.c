6387ec23fd93d0bf7a81f365cad516b10681b769
erickbrizuela <alanocturna44@gmail.com>
2025-01-29 11:29:47 -0600

    comentario , mi primer actualizacion

---
+++ Jan29/programa_hola.c
@@ -0,0 +1,6 @@
+/*
+Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por 
+ejemplo:
+Este programa imprimirá la típica frase “hola mundo” a la pantalla
+Creado el 29/01/2025 por erick bl
+*/
