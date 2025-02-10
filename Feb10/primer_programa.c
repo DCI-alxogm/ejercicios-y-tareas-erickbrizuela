df573cea1663272b5eb0fc0aa39506ae787172e9
alxogm <alma.gonzalez@fisica.ugto.mx>
2025-02-10 11:30:59 -0600

    primer programa

---
+++ Feb10/primer_programa.c
@@ -0,0 +1,12 @@
+
+/*Creado el 10 de Ferbero del 2025 por alma gonzalez*/
+
+#include<stdio.h>
+
+int main(){
+
+	printf("Hola Alma \n");
+	printf("Este es el primer programa del curso\n");
+}
+
+/*Termine de escribir el programa*/
